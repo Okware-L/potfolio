74d50d3819e011f4f887367662ba4fdb2b2028f4
unknown <lewisokware@gmail.com>
2022-07-17 12:40:49 +0300

    commit to github

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Main from '../components/Main'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Okware |web3 Developer</title>
+        <meta name="description" content="I’m a web3 developer :)." />
+        <link rel="icon" href="/fav.png" />
+      </Head>
+    <Main />
+    <About />
+    <Skills />
+    <Projects />
+    <Contact />
+    </div>
+  )
+}
