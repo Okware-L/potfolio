6e0c8c0e2ff100a4585273c8bf45c5a76d86f3fe
unknown <lewisokware@gmail.com>
2022-07-17 15:59:27 +0300

    changes

--- components/Main.jsx
+++ components/Main.jsx
@@ -17,7 +17,7 @@ const Main = () => {
           </h1>
           <h1 className='py-2 text-gray-700'>A Web3 Developer</h1>
           <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
-            I&#39;m focused on building web3 dapps & responsive front-end web applications while
+            I focus on building web3 dapps & responsive front-end web applications while
             learning solidity.
           </p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
